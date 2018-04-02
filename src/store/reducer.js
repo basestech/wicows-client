b019ca0a3c888b72e0aa3fa4d4d2515e561772de
Ali Sivgin <ali.svgn@gmail.com>
2018-04-02 14:32:25 +0300

    Added values to store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,24 @@
+import * as actionTypes from './actions';
 
+const initialState = {
+    animals: {
+        ids : [1,2,3],
+        byId:{
+            1: {id: 1, name :'Sarıkız', number : 52},
+            2: {id: 2, name :'Karakız', number : 53},
+            3: {id: 3, name :'Alakız', number : 54}
+        }
+    },
+    conditions : [
+        
+    ],
+    actions : []
+}
 
-const reducer = ()=>{
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        
+    }
 
 }
 
