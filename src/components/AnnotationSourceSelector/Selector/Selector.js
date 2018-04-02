40c87b54b9e501cdfe85a5fa9e72d84026b38975
Ali Sivgin <ali.svgn@gmail.com>
2018-04-02 12:57:55 +0300

    Added error handle to Selector
    Handled in case of no source exists

--- src/components/AnnotationSourceSelector/Selector/Selector.js
+++ src/components/AnnotationSourceSelector/Selector/Selector.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Selector = ({options})=>{
     return <select>
-                {
+                {   options ? 
                     options.map((val)=>{
                         return <option value={val.value} onchanged= {val.changed} >{val.text}</option>;
                     })
+                    :
+                    <option>No source for select</option>
                 }  
            </select>
 }
