bf575ea07c3d9bbf2f18ca45bd7182e1ea139368
Ali Sivgin <ali.svgn@gmail.com>
2018-04-02 12:54:29 +0300

    AnnotationView Added

---
+++ src/containers/AnnotationView/AnnotationView.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import AnnotationHeader from '../../components/AnnotationHeader/AnnotationHeader';
+import Annotation from '../../components/Annotation/Annotation';
+import AnnotationSourceSelector from '../../components/AnnotationSourceSelector/AnnotationSourceSelector'
+
+class AnnotationView extends Component{
+    state = {
+
+    }
+
+    render(){
+        return (
+            <React.Fragment>
+                <AnnotationHeader 
+                    actionType = 'insemination' 
+                    animalName = 'Sarıkız' 
+                    animalNumber = '52'
+                />
+                <Annotation label = 'Test Annotation'/>
+                <AnnotationSourceSelector />
+            </React.Fragment>
+        );
+    }
+}
+
+
+const mapStateToProps= state =>{
+    return {
+
+    };
+}
+
+const mapDispatchToProps = dispatch =>{
+    return {
+    
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnnotationView);
