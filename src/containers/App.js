ccaa997cb820afa9f39001d3ee6323d66e057204
Ali Sivgin <ali.svgn@gmail.com>
2018-04-02 14:29:12 +0300

    rendering AnnotationView

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,12 +4,14 @@ import logo from '../logo.svg';
 import './App.css';
 
 import AnimalHeader from '../components/AnimalHeader/AnimalHeader';
+import AnnotationView from './AnnotationView/AnnotationView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       
+        <AnnotationView />
       </div>
     );
   }
