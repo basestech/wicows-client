0d82afc0167b4238c6bbf9a2ad79567d90eb08d9
Ali Sivgin <ali.svgn@gmail.com>
2018-04-02 14:24:15 +0300

    comment out react-logo

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+//import logo from '../logo.svg';
 import './App.css';
 
 import AnimalHeader from '../components/AnimalHeader/AnimalHeader';
