753c4133eec8273f8841e6b5ec4f166f160f2c9a
Ali Sivgin <ali.svgn@gmail.com>
2018-04-03 15:43:05 +0300

    Edited react store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,22 +4,24 @@ const initialState = {
     animals: {
         ids : [1,2,3],
         byId:{
-            1: {id: 1, name :'Sarıkız', number : 52},
-            2: {id: 2, name :'Karakız', number : 53},
-            3: {id: 3, name :'Alakız', number : 54}
+            1: {name :'Sarıkız', number : 52},
+            2: {name :'Karakız', number : 53},
+            3: {name :'Alakız', number : 54}
         }
     },
     conditions : [
-        
+        {id : 1, name : 'Hastalık'}
     ],
-    actions : []
+    actions : {
+        1: {animal_id :2, annotation_label: 'Annotation about Animal given', type : 'insemination' , conditions: [1]}
+    }
 }
 
 const reducer = (state = initialState, action)=>{
     switch(action.type){
         
     }
-
+    return state;
 }
 
 export default reducer;
