7f522abe6de2c68ea4abece569c21b6ba578b8c8
Ali Sivgin <ali.svgn@gmail.com>
2018-04-03 15:46:23 +0300

    Recommitted App.js
    Due to unneccessary lines

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 //import logo from '../logo.svg';
 import './App.css';
 
-import AnimalHeader from '../components/AnimalHeader/AnimalHeader';
 import AnnotationView from './AnnotationView/AnnotationView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
         <AnnotationView />
       </div>
     );
