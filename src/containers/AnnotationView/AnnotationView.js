4d56cc077dfb85b1dfa4c49fe4eb78bf3440e636
Ali Sivgin <ali.svgn@gmail.com>
2018-04-03 15:48:00 +0300

    Rendering AnnotationView by state
    Getting informations from redux store

--- src/containers/AnnotationView/AnnotationView.js
+++ src/containers/AnnotationView/AnnotationView.js
@@ -4,37 +4,56 @@ import { connect } from 'react-redux';
 import AnnotationHeader from '../../components/AnnotationHeader/AnnotationHeader';
 import Annotation from '../../components/Annotation/Annotation';
 import AnnotationSourceSelector from '../../components/AnnotationSourceSelector/AnnotationSourceSelector'
+import SubmitButton from '../../components/SubmitButton/SubmitButton'
 
 class AnnotationView extends Component{
     state = {
+        action_id : null
+    }
 
+    componentWillMount(){
+        //this.state = {...this.state, action_id :1};
+        this.setState({action_id :1});
     }
 
     render(){
+        let annotationView= <p>No Action</p>;
+
+        if(this.props._actions[this.state.action_id]){
+            let action = this.props._actions[this.state.action_id];
+            console.log(action);
+
+            annotationView = (
+                <React.Fragment>
+                    <AnnotationHeader 
+                        actionType = {action.type}
+                        animalName = {this.props._animals.byId[action.animal_id].name} 
+                        animalNumber = {this.props._animals.byId[action.animal_id].number}
+                    />
+                    <Annotation label = {action.annotation_label}/>
+                    {/* <AnnotationSourceSelector options={
+                        [{value : 'value', changed: '', text: 'Annotation 1'}]
+                    }/> */}
+                    <SubmitButton/>
+                </React.Fragment>
+            )
+
+        }
+
+
         return (
             <React.Fragment>
-                <AnnotationHeader 
-                    actionType = 'insemination' 
-                    animalName = 'Sarıkız' 
-                    animalNumber = '52'
-                />
-                <Annotation label = 'Test Annotation'/>
-                <AnnotationSourceSelector />
-                <AnnotationSourceSelector options={
-                    [
-                        {value : 'value', changed: '', text: 'Annotation 1'}
-                    ]
-                }/>
-                <SubmitButton/>
+                {annotationView}
             </React.Fragment>
         );
     }
 }
 
-
 const mapStateToProps= state =>{
     return {
-
+        _animals : state.animals,
+        _conditions : state.conditions,
+        _actions : state.actions
     };
 }
 
