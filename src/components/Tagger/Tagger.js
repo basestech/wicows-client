e1e1c8fee6ad8cb6a3c3c7e4e672b89f55985ce9
Mert Torun <mert@merttorun.com>
2018-04-06 19:40:28 +0300

    default empty selected array for <Tagger>

--- src/components/Tagger/Tagger.js
+++ src/components/Tagger/Tagger.js
@@ -1,20 +1,20 @@
 import  React  from 'react';
 
-const Tagger = ({onChange, tags, selected}) =>{
+const Tagger = ({onChange, tags, selected=[]}) =>{
     return tags.map((val) =>{
         const changeHandler = (e)=>{
             onChange(val.id);
         }
         return <React.Fragment key = {val.id}>
-                    <input 
-                    id = {val.id} 
-                    type = "checkbox" 
+                    <input
+                    id = {val.id}
+                    type = "checkbox"
                     onChange = {changeHandler}
                     defaultChecked = {selected.includes(val.id) ? 'checked' : null}
                     />
                     <label htmlFor= {val.id}>{val.name}</label>
                 </React.Fragment>
-            
+
     });
 }
 
