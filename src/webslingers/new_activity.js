c4eab50ea1c32ab34a59ddc0c87256b0c9701eb8
Mert Torun <mert@merttorun.com>
2018-04-06 17:22:02 +0300

    webslinger prototype resolves instead

--- src/webslingers/new_activity.js
+++ src/webslingers/new_activity.js
@@ -6,7 +6,7 @@ function new_activity_slinger() {
     const status = store.getState().new_activity.status;
     if (status === WEB.REQUEST) {
         new Promise(function(resolve, reject) {
-            setTimeout(reject, 1500, 'foo');
+            setTimeout(resolve, 1500, 'foo');
         }).then(()=>{
             store.dispatch({ type: NEW_ACTIVITY_SUCCESS });
         }).catch((err)=>{
