853858f786c0ab1ef0a48c28c4eb1bd351de9df9
Mert Torun <mert@merttorun.com>
2018-04-06 15:59:44 +0300

    slinger prototype for web requests

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
-import logger from './store/logger'
+import new_activity_slinger from './webslingers/new_activity'
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
 
-//store.subscribe(logger);
+store.subscribe(new_activity_slinger);
 
 export {store};
---
+++ src/webslingers/new_activity.js
@@ -0,0 +1,18 @@
+import { store } from '../index'
+import * as WEB from '../constants/webStatus'
+import { NEW_ACTIVITY_SUCCESS, NEW_ACTIVITY_FAILURE } from '../store/actions'
+
+function new_activity_slinger() {
+    const status = store.getState().new_activity.status;
+    if (status === WEB.REQUEST) {
+        new Promise(function(resolve, reject) {
+            setTimeout(reject, 1500, 'foo');
+        }).then(()=>{
+            store.dispatch({ type: NEW_ACTIVITY_SUCCESS });
+        }).catch((err)=>{
+            store.dispatch({ type: NEW_ACTIVITY_FAILURE, error: err });
+        });
+    }
+}
+
+export default new_activity_slinger
