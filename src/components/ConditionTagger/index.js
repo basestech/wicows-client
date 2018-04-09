8fadeb7115bcf0553398c0fc7071078c8502a476
Mert Torun <mert@merttorun.com>
2018-04-09 17:30:32 +0300

    CSS and structure for <ConditionTagger>

--- src/components/ConditionTagger/index.js
+++ src/components/ConditionTagger/index.js
@@ -3,8 +3,12 @@ import  React  from 'react';
 import conditions from '../../constants/conditions';
 import Tagger from '../Tagger';
 
+import './ConditionTagger.css'
+
 const ConditionTagger = (props) => {
-    return <Tagger tags={conditions} {...props}/>
+    return <ul className="tagger condition">
+        {Tagger({...props, tags: conditions }).map( tag => <li key={tag.key}>{tag}</li> )}
+    </ul>
 }
 
 export default ConditionTagger;
--- src/components/Tagger/index.js
+++ src/components/Tagger/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tagger = ({onChange, tags, selected=[]}) =>{
     return tags.map((val) =>{
@@ -6,16 +7,26 @@ const Tagger = ({onChange, tags, selected=[]}) =>{
             onChange(val.id);
         }
         return <React.Fragment key = {val.id}>
-                    <input
-                    id = {val.id}
-                    type = "checkbox"
-                    onChange = {changeHandler}
-                    defaultChecked = {selected.includes(val.id) ? 'checked' : null}
-                    />
-                    <label htmlFor= {val.id}>{val.name}</label>
-                </React.Fragment>
-
+            <input
+                id = {val.id}
+                type = "checkbox"
+                onChange = {changeHandler}
+                defaultChecked = {selected.includes(val.id) ? 'checked' : null}
+            />
+            <label htmlFor={val.id}>{val.name}</label>
+        </React.Fragment>
     });
+}
+
+Tagger.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    tags: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+        }).isRequired
+    ).isRequired,
+    selected: PropTypes.arrayOf(PropTypes.string.isRequired),
 }
 
 export default Tagger;
