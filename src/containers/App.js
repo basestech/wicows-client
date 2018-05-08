72de902e0ed63c50f4abaeeb53e534fbbdc782b4
Mert Torun <mert@merttorun.com>
2018-05-08 16:38:15 +0300

    Simplify <AnnotationView> folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 //import logo from '../logo.svg';
 import "./App.css";
 
-import AnnotationView from "./AnnotationView/AnnotationView";
+import AnnotationView from "./AnnotationView";
 
 class App extends Component {
     render() {
