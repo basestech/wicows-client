89f03e32b9748f4ee5edad754bb9985ad1923d5d
Mert Torun <mert@merttorun.com>
2018-05-09 16:16:05 +0300

    Somewhat better random data
    Small local and larger long fluctuations

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,7 +1,11 @@
-const data = []
+const data = [];
+let prevy = 0;
+let dev = 0;
 
 for (let x = 2; x <= 300; x++) {
-  data.push({x: x, y: Math.floor(Math.random() * 90)})
+    dev = Math.floor(Math.random() * (90 - prevy) / 10 - (dev / 2 + 3));
+    data.push({ x: x, y: prevy + dev });
+    prevy = data[data.length - 1].y;
 }
-
+debugger;
 export default data;
